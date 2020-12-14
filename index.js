a96007225a6e140163d34f4bddbc55c860858760
sntwandy <sntwandy@gmail.com>
2020-12-14 11:29:35 -0400

    Understanding the resolvers, who executes de query

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const { graphql, buildSchema } = require('graphql');
 const schema = buildSchema(`
   type Query {
     hello: String
+    children: String
   }
 `);
 
+// define the resolvers, who execute the query
+const resolvers = {
+  hello: () => {
+    return 'I love your Megan';
+  },
+  children: () => {
+    return 'Darcy, Zeus and Atenas...'
+  }
+}
+
+
 // execute query 'hello'
-graphql(schema, '{ hello }')
+graphql(schema, '{ children, hello }', resolvers)
   .then(data => console.log(data))
   .catch(error => console.log(error));
