ec4ba7800ea647a66f310d9ac3717986312e06a1
sntwandy <sntwandy@gmail.com>
2020-12-14 11:48:04 -0400

    Serving our app with express and express-graphql middleware

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 'use strict'
 
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql'); // graphql middleware for express
 const { graphql, buildSchema } = require('graphql');
+
+// initializing express
+const app = express();
+const port = process.env.port || 3000; // defining the port
 
 // define the schema
 const schema = buildSchema(`
   type Query {
+    "Return a greeting"
     hello: String
+    "Return our children names"
     children: String
   }
 `);
@@ -17,11 +25,22 @@ const resolvers = {
   },
   children: () => {
     return 'Darcy, Zeus and Atenas...'
-  }
-}
+  },
+};
 
+// defining the middleware into express
+app.use('/api', graphqlHTTP({
+  schema: schema, // schema of the all the App
+  rootValue: resolvers, // Resolvers to the app use
+  graphiql: true, // enable the graphiql enviroment
+}));
 
-// execute query 'hello'
+app.listen(port, () => {
+  console.log(`The server is listening at port: http://localhost:${port}/api`);
+});
+
+/* execute query 'hello', in the terminal
 graphql(schema, '{ children, hello }', resolvers)
   .then(data => console.log(data))
   .catch(error => console.log(error));
+*/
